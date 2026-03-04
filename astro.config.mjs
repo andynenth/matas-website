c0cc50cb9b62eae0a276349df84408b4d86c9098
c0516154 <andy.nenth@gmail.com>
2026-03-04 09:56:03 -0800

    remove tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://mataslawoffice.com',
   i18n: {
     defaultLocale: 'th',
     locales: ['th', 'en'],
